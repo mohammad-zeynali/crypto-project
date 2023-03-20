0e1d1de309021fc2175f9683b5ebff5fd50ff42e
Mohammad Zeynali <mhmdzynali@gmail.com>
2023-03-20 06:02:31 +0330

    crypto project

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -74,7 +74,7 @@ class Navbar extends Component {
                             <li className="dropdown-menu__item"><a href="#" className="dropdown-menu__link">صفحه ثبت نام</a></li>
                         </ul>
                     </li>
-                    <li onClick={this.menuClickHandler.bind(this)} className="menu__item menu__item-mobile-down"><a href="#" className="menu__link menu__link--down">صفحات
+                    <li onClick={this.menuClickHandler.bind(this)} className={`menu__item menu__item-mobile--down ${this.state.isDropdownMenu ? "menu__item-mobile--open" : ""}`}><a href="#" className="menu__link menu__link--down">صفحات
                             <svg className="menu__down-icon" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M14 7L9 12L14 17" stroke="#637381" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
                             </svg>
